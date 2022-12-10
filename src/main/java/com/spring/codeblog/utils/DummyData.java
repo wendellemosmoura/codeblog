b044be3a90cf62e1018ca481b0b5a8ad99a6ca45
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2022-12-10 10:21:57 -0300

    Criação do model, repository, service, serviceImpl e salvando dois posts utilizando a classe DummyData, no package utils.

--- src/main/java/com/spring/codeblog/utils/DummyData.java
+++ src/main/java/com/spring/codeblog/utils/DummyData.java
@@ -2,7 +2,6 @@ package com.spring.codeblog.utils;
 
 import com.spring.codeblog.models.Post;
 import com.spring.codeblog.repositories.CodeblogRepository;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
