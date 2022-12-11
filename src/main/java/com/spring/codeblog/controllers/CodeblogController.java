a60a4b5830aaaae3641208fc9f63828ad2109940
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2022-12-10 21:51:11 -0300

    Criação das rotas e páginas html para exibição de todos os posts e de detalhes de um post.

---
+++ src/main/java/com/spring/codeblog/controllers/CodeblogController.java
@@ -0,0 +1,35 @@
+package com.spring.codeblog.controllers;
+
+import com.spring.codeblog.models.Post;
+import com.spring.codeblog.services.CodeblogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CodeblogController {
+
+    @Autowired
+    CodeblogService codeblogService;
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts() {
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = codeblogService.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post);
+        return mv;
+    }
+}
